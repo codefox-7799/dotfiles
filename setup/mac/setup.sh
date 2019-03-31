75385a5fc25b3b4655265d920103cfee16e31496
Nobuchika Tanaka <codefox.7799@gmail.com>
2019-04-01 06:28:18 +0900

    Install google-chrome into mac

--- setup/mac/setup.sh
+++ setup/mac/setup.sh
@@ -5,3 +5,6 @@ ${HOME}/dotfiles/setup/mac/install_cmdlinetools.sh
 
 # Install homebrew
 ${HOME}/dotfiles/setup/mac/install_homebrew.sh
+
+# Install apps with homebrew
+${HOME}/dotfiles/setup/mac/brew.sh
